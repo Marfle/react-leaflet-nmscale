f9cd3c0a6576af4b9e85e5501b9adb46824851a3
Antti Lehto <anajavi@users.noreply.github.com>
2021-11-19 11:34:28 +0200

    test: rewrite tests with @testing-library/react

--- __tests__/nmscale.tsx
+++ __tests__/nmscale.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { renderIntoDocument } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
 
 import { MapContainer, TileLayer } from 'react-leaflet';
 
@@ -7,24 +7,45 @@ import { NmScale } from '../src/nmscale';
 import LeafletNmScale from '../src/leafletnmscale';
 
 describe('NmScale', () => {
-  it('adds the scale to the map', (done) => {
-    const spy_updateNautical = jest.spyOn(
+  let spy_updateNautical;
+
+  beforeEach(() => {
+    spy_updateNautical = jest.spyOn(
       LeafletNmScale.prototype,
       '_updateNautical'
     );
+  });
 
-    renderIntoDocument(
-      <MapContainer
-        center={[0, 0]}
-        zoom={10}
-        whenCreated={() => {
-          expect(spy_updateNautical).toHaveBeenCalledTimes(1);
-          done();
-        }}
-      >
+  afterEach(() => {
+    spy_updateNautical.mockRestore();
+  });
+
+  it('adds the scale to the map', () => {
+    const { container } = render(
+      <MapContainer center={[0, 0]} zoom={10}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <NmScale />
       </MapContainer>
     );
+
+    const scalediv = container.querySelector('.leaflet-control-scale');
+    expect(scalediv).not.toBe(null);
+
+    expect(spy_updateNautical).toHaveBeenCalledTimes(1);
+  });
+
+  it('removes the scale when unmounted', () => {
+    const Component = ({ mount = true }: { mount: boolean }) => (
+      <MapContainer center={[0, 0]} zoom={10}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {mount ? <NmScale /> : null}
+      </MapContainer>
+    );
+
+    const { rerender, container } = render(<Component mount={true} />);
+    rerender(<Component mount={false} />);
+
+    const scalediv = container.querySelector('.leaflet-control-scale');
+    expect(scalediv).toBe(null);
   });
 });

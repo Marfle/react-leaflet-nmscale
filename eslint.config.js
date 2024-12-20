bf11e907df936236a91e9d624a883e0c1ea6f1f8
Antti Lehto <anajavi@users.noreply.github.com>
2024-12-20 16:52:38 +0200

    test: remove eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
 import js from '@eslint/js';
 import globals from 'globals';
 
 const config = [
   js.configs.recommended,
-  reactPlugin.configs.flat.recommended,
   ...tseslint.configs.recommended,
   {
     files: ['__tests__/**'],
